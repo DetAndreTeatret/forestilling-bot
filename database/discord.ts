54b616df6d3e6523fdf1b32e8d8239087f6a85e2
KingOfSquares <19822231+KingOfSquares@users.noreply.github.com>
2023-06-13 11:20:01 +0200

    Start work on database

--- database/discord.ts
+++ database/discord.ts
@@ -1,12 +1,13 @@
 import {GuildChannel, Snowflake} from "discord.js";
 import {User} from "./user.js";
+import {addEntry, selectEntries} from "./sqlite";
 
 
 /**
  * Store a time for when a {@link GuildChannel} can be deleted
  */
 export async function queChannelDeletion(channel: GuildChannel, when: Date) {
-    //Add point of deletion for given channel(snowflake)
+    await addEntry("DiscordChannelDeletions", String(when.getTime()), channel.id)
 }
 
 /**
@@ -24,7 +25,7 @@ export async function getDeleteableChannels() {
  * Store a time for when a {@link User} can be removed from a {@link GuildChannel}
  */
 export async function cueUserRemovalFromDiscord(user: User, channel: GuildChannel, when: Date) {
-
+    await addEntry("DiscordUserRemovals", String(when.getTime()), channel.id, user.discord.member.id)
 }
 
 /**
@@ -33,7 +34,8 @@ export async function cueUserRemovalFromDiscord(user: User, channel: GuildChanne
  * @return an array of member id snowflakes containing all members that can be deleted from the given channel
  */
 export async function getRemovableUsers(channel: GuildChannel) {
-    const result: Snowflake[] = []
+    const result = selectEntries("DiscordUserRemovals", "DiscordChannelSnowflake...UnixEpoch > " + new Date().getTime(), ["DiscordChannelSnowflake, DiscordUserSnowflake"])
+    const result0: Snowflake[] = []
 
-    return result
+    return result0
 }
--- database/sqlite.ts
+++ database/sqlite.ts
@@ -1,15 +1,44 @@
 import sqlite3 from "sqlite3";
-import { open } from 'sqlite'
+import {open} from 'sqlite'
 import path from "node:path"
 
 sqlite3.verbose() //TODO: enable on startup argument
+
 
 const db = await open({
     filename: path.join(__dirname, "database.db"),
     driver: sqlite3.cached.Database
 })
 
-export async function queryDatabase() {
+/** Database layout (C="Column")
+ *
+ * TABLE 1: Users, C1 DiscordUserSnowflake, C2 SchedgeUpID, C3 Display name(From SU)
+ *
+ * TABLE 2: DiscordChannelDeletions, C1 UnixEpoch, C2 DiscordChannelSnowflake
+ *
+ * TABLE 3: DiscordUserRemovals, C1 UnixEpoch, C2 DiscordChannelSnowflake, C3 DiscordUserSnowflake
+ *
+ */
+export async function createTables() {
+    await db.exec("CREATE TABLE IF NOT EXISTS Users(DiscordUserSnowflake BIGINT UNSIGNED, SchedgeUpID INT, DisplayName varchar(255))")
+    await db.exec("CREATE TABLE IF NOT EXISTS DiscordChannelDeletions(UnixEpoch TIMESTAMP, DiscordChannelSnowflake BIGINT UNSIGNED)")
+    await db.exec("CREATE TABLE IF NOT EXISTS DiscordUserRemovals(UnixEpoch TIMESTAMP, DiscordChannelSnowflake BIGINT UNSIGNED, DiscordUserSnowflake BIGINT UNSIGNED)")
+}
 
+type DatabaseTables = "Users" | "DiscordChannelDeletions" | "DiscordUserRemovals"
+
+/**
+ * Method caller is responsible for the amount and order of params, such that it matches the column layout of the table specified
+ * @param table The table to insert an entry into
+ * @param params The values of the entry, in the order specified in the Database Layout
+ * @see createTables
+ */
+export async function addEntry(table: DatabaseTables, ...params: string[]) {
+    await db.exec("INSERT INTO " + table + " VALUES(" + params + ")")
+}
+
+export async function selectEntries(table: DatabaseTables, condition: string, columns?: string[]) {
+    const columnString = columns == undefined ? "*" : "(" + columns + ")"
+    return await db.get("SELECT * FROM " + table + " WHERE " + condition)
 }
 
