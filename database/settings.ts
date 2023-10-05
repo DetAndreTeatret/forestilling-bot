7a612720aff2c6f7413f402ade24b40b3ff7c462
KingOfSquares <19822231+KingOfSquares@users.noreply.github.com>
2023-10-05 21:25:19 +0200

    Enforce setting key length

--- database/settings.ts
+++ database/settings.ts
@@ -5,6 +5,7 @@ import {addEntry, selectEntry, updateEntry} from "./sqlite.js"
  * @param key the setting key
  */
 export async function fetchSetting(key: string): Promise<string | undefined> {
+    assertValidSettingKey(key)
     const result = await selectEntry("Settings", "SettingKey=\"" + key + "\"", ["SettingValue"])
     if(result === undefined) return undefined
     else return result["SettingValue"]
@@ -15,6 +16,7 @@ export async function fetchSetting(key: string): Promise<string | undefined> {
  * @param key the setting key
  */
 export async function needSetting(key: string) {
+    assertValidSettingKey(key)
     const result = await fetchSetting(key)
     if(result === undefined) throw Error("Needed a value for setting " + key + ", but none was found.")
     else return result
@@ -27,10 +29,17 @@ export async function needSetting(key: string) {
  * @param value the new setting value
  */
 export async function updateSetting(key: string, value: string) { // TODO: Should trigger a refresh of the necessary code to use the new value
+    assertValidSettingKey(key)
     const result = await fetchSetting(key)
     if(result === undefined) {
         await addEntry("Settings", "'" + key + "'", "'" + value + "'")
     } else {
         await updateEntry("Settings", "SettingKey=\"" + key + "\"", "SettingValue", value)
+    }
+}
+
+function assertValidSettingKey(value: string) { // TODO create some regex to make predictable keys
+    if(value.length > 60) {
+        throw new Error("Setting key " + value + " is too long! (max 60 chars)")
     }
 }
