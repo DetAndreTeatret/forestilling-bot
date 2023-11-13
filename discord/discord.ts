5bb4d59976028b1347a59da1363b922b402e0b57
KingOfSquares <19822231+KingOfSquares@users.noreply.github.com>
2023-11-13 17:54:04 +0100

    update schedgeup-scraper

--- discord/discord.ts
+++ discord/discord.ts
@@ -306,7 +306,7 @@ export async function updateShowsInEventInfoMessage(channel: TextChannel, eventD
 function createEventInfoEmbed(eventDate: Date, shows: string) {
     const embedBuilder = new EmbedBuilder()
     embedBuilder.setTitle("Kanal for " + getDayNameNO(eventDate) + "s forestillinger(" + shows + ")")
-    embedBuilder.setDescription("Velkommen til denne kanalen, ha en fin uke videre! :sunglasses:\nOBS: Husk at denne kanalen forsvinner når forestillingen er over!")
+    embedBuilder.setDescription("Velkommen til denne kanalen, ha en fin uke videre! :sunglasses:\nOBS: Husk at kanalen forsvinner når forestillingen er over!")
     embedBuilder.addFields({name: "Husk å bestille mat!", value: "https://bit.ly/DATMAT"})
     embedBuilder.setColor("Random")
     embedBuilder.setImage("https://www.detandreteatret.no/uploads/assets/images/Stemning/_800x800_crop_center-center_82_none/andre-teatret-logo.png")
