9bd0f0f5e0607a12de3462ed09d6f2c7a167e0fa
KingOfSquares <19822231+KingOfSquares@users.noreply.github.com>
2024-04-02 12:36:03 +0200

    Change formatting for userstats unlinked
    Now displays unlinked members categorized by their highest Role

--- src/discord/commands/userStats.ts
+++ src/discord/commands/userStats.ts
@@ -30,8 +30,16 @@ async function unlinkedUserStats(interaction: ChatInputCommandInteraction) {
     const guild = needNotNullOrUndefined(interaction.guild, "unlinkedUserStats#guild")
     const members = Array.from((await guild.members.fetch()).values())
     const users = await fetchAllUsers()
-    const unlinkedMembers = members.filter(m => users.find(u => u.discordSnowflake === m.id) === undefined)
-    const unlinkedMembersText = unlinkedMembers.map(m => m.user.toString()).join(", ")
+    const unlinkedMembers = members.filter(m => users.find(u => u.discordSnowflake === m.id) === undefined && !m.user.bot)
+
+    const discordRoles = Array.from((await guild.roles.fetch()).entries())
+    let unlinkedMembersText = ""
+    for await (const discordRole of discordRoles) {
+        const membersWithRole = unlinkedMembers.filter(m => m.roles.highest.id === discordRole[0])
+        if (membersWithRole.length === 0) continue
+        unlinkedMembersText += "\n" + discordRole[1].name + ":\n" + membersWithRole.map(m => m.toString()).join(", ")
+    }
+
     await logger.logLine("Discord users that are currently not linked to an user:")
     await logger.logLine(unlinkedMembersText)
 
